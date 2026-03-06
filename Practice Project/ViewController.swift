aae73e5e926e36b45e1355406ba66550bcc644d8
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-03-06 11:56:27 +0530

    First local comitt

--- Practice Project/ViewController.swift
+++ Practice Project/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        debugPrint("working")
         // Do any additional setup after loading the view.
     }
 
