bd13ca7eeb35d877a5c6fca74de7d269acb3b9c2
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-03-09 19:02:25 +0530

    Done with crown UI top part.

--- Practice Project/ViewController.swift
+++ Practice Project/ViewController.swift
@@ -9,12 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        debugPrint("working")
-        // Do any additional setup after loading the view.
-    }
+    
 
+        addCrown()
+        //addFeature()
+    }
+    
+    private func addCrown() {
+
+        let crownVC = CrownControlViewController()
+
+        addChild(crownVC)
+        view.addSubview(crownVC.view)
+
+        crownVC.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            crownVC.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            crownVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            crownVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            crownVC.view.heightAnchor.constraint(equalToConstant: 450)
+        ])
+
+        crownVC.didMove(toParent: self)
+    }
 
 }
 
+//        let crownVC = CrownControlViewController()
+//
+//           addChild(crownVC)
+//
+//           view.addSubview(crownVC.view)
+//
+//           crownVC.view.translatesAutoresizingMaskIntoConstraints = false
+//
+//           NSLayoutConstraint.activate([
+//               crownVC.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//               crownVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//               crownVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//               crownVC.view.heightAnchor.constraint(equalToConstant: 420)
+//           ])
+//
+//           crownVC.didMove(toParent: self)
+
+
+
+
+//      private func addFeature() {
+//
+//          let featureVC = featureViewController()
+//
+//          addChild(featureVC)
+//          view.addSubview(featureVC.view)
+//
+//          featureVC.view.frame = CGRect(
+//              x: 0,
+//              y: view.frame.height/2,
+//              width: view.frame.width,
+//              height: view.frame.height
+//          )
+//
+//          featureVC.didMove(toParent: self)
+//      }
---
+++ Practice Project/featureViewController.swift
@@ -0,0 +1,37 @@
+//
+//  featureViewController.swift
+//  Practice Project
+//
+//  Created by Tharik anver  on 06/03/26.
+//
+
+import UIKit
+
+class featureViewController: UIViewController {
+
+//    init() {
+//           super.init(nibName: "featureViewController", bundle: nil)
+//       }
+//
+//       required init?(coder: NSCoder) {
+//           super.init(coder: coder)
+//       }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
